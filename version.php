6525365468f6ecc46af7c4d4b91c6f90d667437c
Dom Sinclair <dom@viewtolearn.co.uk>
2025-12-23 15:14:28 +0000

    fix: Another version fix to release-please

--- version.php
+++ version.php
@@ -5,4 +5,4 @@
  * This file provides a single source of truth for the framework version used by the CLI
  * and documentation. Bump as part of release tagging.
  */
-return '0.7.0';
+return trim(file_get_contents(__DIR__ . '/VERSION'));
